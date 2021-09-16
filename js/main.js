d5c8361ad73c444a7bc1a80d2343e05bdbd95b27
Azubuike Chibueze Golden <60342841+Goldenhub@users.noreply.github.com>
2021-09-16 23:54:51 +0100

    added already developed html, css and js files

---
+++ js/main.js
@@ -0,0 +1,181 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let tableBody = document.querySelector('#tbody');
+
+    let dataButtons = document.querySelectorAll('button[data-btn]');
+
+    dataButtons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            fetchDataForLineChart(e.currentTarget.dataset.btn);
+        })
+    });
+
+    fetchDataForLineChart("all");
+    fetchDataForPieChart();
+
+
+    async function fetchDataForLineChart(period) {
+        let fetchurl = await fetch(`https://disease.sh/v3/covid-19/historical/all?lastdays=${period}`);
+        let resp = await fetchurl.json();
+
+       Highcharts.chart({
+        chart: {
+            renderTo: 'container',
+            type: 'line',
+            style: {
+                fontFamily: 'monospace',
+            },
+            backgroundColor: 'black',
+            scrollablePlotArea: {
+                minWidth: 700,
+                scrollPositionX: 1
+            }
+        },
+        title: {
+            text: 'COVID-19 HISTORICAL REPORT',
+            style: {
+                color: '#fff'
+            }
+        },
+        xAxis: {
+            labels: {
+            overflow: 'justify',
+            },
+            categories: Object.keys(resp.cases),
+            min: 0,
+            max: 19,
+            scrollbar: {
+                enabled: true
+          }
+        },
+        yAxis: {
+            title: {
+                text: 'CASES',
+                style: {
+                    color: '#fff'
+                }
+            }
+        },
+        tooltip: {
+            split: true
+        },
+        plotOptions: {
+            line: {
+                lineWidth: 3,
+                states: {
+                hover: {
+                    lineWidth: 6
+                }
+                },
+                marker: {
+                    enabled: false
+                },
+            }
+        },
+        legend: {
+            color: 'white',
+            backgroundColor: 'grey'
+        },      
+        series: [
+            {
+                name: 'cases',
+                color: '#ee0',
+                data: Object.values(resp.cases),
+            }, 
+            {
+                name: 'deaths',
+                color: '#f00',
+                data: Object.values(resp.deaths)
+            },
+            {
+                name: 'recovered',
+                data: Object.values(resp.recovered),
+                color: '#0f0'
+            }
+        ]
+    });
+       return 'Data fetched';
+    
+    }
+
+    async function fetchDataForPieChart() {
+
+        let fetchurl = await fetch(`https://disease.sh/v3/covid-19/all`);
+        let resp = await fetchurl.json();
+
+        tableBody.innerHTML = `
+            <tr>
+                <td>Cases</td>
+                <td>${resp.cases}</td>
+            </tr>
+            <tr>
+                <td>Active</td>
+                <td>${resp.active}</td>
+            </tr>
+            <tr>
+                <td>Recovered</td>
+                <td>${resp.recovered}</td>
+            </tr>
+            <tr>
+                <td>Deaths</td>
+                <td>${resp.deaths}</td>
+            </tr>
+        `
+
+        Highcharts.chart('pie', {
+            chart: {
+                style: {
+                    fontFamily: 'monospace',
+                },
+                backgroundColor: 'black',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Overview'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        color: '#fff',
+                    }
+                }
+            },
+            series: [{
+                name: 'Count',
+                colorByPoint: true,
+                data: [{
+                    name: 'Active Cases',
+                    color: '#ff0',
+                    y: (resp.active/resp.cases)*100 ,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Recovered',
+                    color: '#444',
+                    y: (resp.recovered/resp.cases)*100
+                }, {
+                    name: 'Death',
+                    color: '#d00',
+                    y: (resp.deaths/resp.cases)*100
+                }]
+            }]
+        });
+
+        return 'data fetched';
+    }
+    
+});
+
+
+
+
